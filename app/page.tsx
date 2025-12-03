d455159ccdafbe1210febd63824e734daae6c17b
Ryutera <kuya9823@gmail.com>
2025-12-03 10:59:19 +0000

    feat; input field added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 import HeaderAuth from "@/components/HeaderAuth";
+import TextInputField from "@/components/TextInputField";
 
 
 export default function Home() {
+
   return (
-    <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
+    <main className="min-h-screen flex flex-col items-center w-full bg-red-50">
+     
       <HeaderAuth/>
-      </div>
+<div className="mt-20 mb-10">
+  <h1>日本語の自然な表現をAIが翻訳!</h1>
+</div>
+     <TextInputField/>
+     
+     
     </main>
   );
 }
---
+++ components/TextInputField.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { useState } from "react";
+
+const TextInputField = () => {
+    const [inputText, setInputText] = useState("")
+    return (
+        <div className="my-10 w-full text-center flex flex-col items-center gap-5">
+            <textarea placeholder="好きな言語で入力..." className="bg-white h-48 md:w-[60%] w-[85%] rounded-lg p-2 outline-none" onChange={(e) => setInputText(e.target.value)} value={inputText} />
+            <button className={`${inputText? "bg-red-300":"bg-slate-300" } md:w-[60%] w-[85%] py-3 rounded-2xl font-semibold text-white  outline-none`}>翻訳する</button>
+        </div>
+
+    )
+}
+
+export default TextInputField

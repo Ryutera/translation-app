e2e5236fde98a605c911e6878da2b2ef8937d270
Ryutera <kuya9823@gmail.com>
2025-12-08 23:05:02 +0000

    language select option removed from Textinputfiled

--- components/TextInputField.tsx
+++ components/TextInputField.tsx
@@ -5,37 +5,15 @@ import { generateTranslation } from "@/app/action";
 import { useLangOpstion } from "@/lib/store/useLangOption";
 
 
+
 const TextInputField = () => {
     const [inputText, setInputText] = useState("")
     const [loading, setLoading] = useState(false)
     const [output, setOutput] = useState(null)
     const selectedLang = useLangOpstion((state) => state.selectedLang)
-    const selectCHN = useLangOpstion((state) => state.selectCHN)
-    const selectKOR = useLangOpstion((state) => state.selectKOR)
-    const selectENG = useLangOpstion((state) => state.selectENG)
-    const selectJPN = useLangOpstion((state) => state.selectJPN)
 
-    const handleSelection = (e: any) => {
-        switch (e.target.value) {
-            case "English":
-                selectENG()
-                break;
 
-            case "Korean":
-                selectKOR()
-                break;
-
-            case "Chinese":
-                selectCHN()
-                break;
-
-            case "Japanese":
-                selectJPN()
-                break;
-        }
-    }
-
-    const handleDatafetch = async () => {
+    const getTranslationData = async () => {
         try {
             setLoading(true)
             const res = await generateTranslation(inputText, selectedLang)
@@ -51,6 +29,8 @@ const TextInputField = () => {
 
     }
 
+
+
     const translation = (output as any)?.output_text.split("Meaning:")[0].replace("Translation (Japanese):", "").trim()
     const meaning = (output as any)?.output_text.split("Meaning:")[1].split("Explanation:")[0].trim()
     const explanations = (output as any)?.output_text.split("Explanation:")[1].split(".")
@@ -60,8 +40,8 @@ const TextInputField = () => {
 
         <div className="my-10 w-full text-center flex flex-col items-center gap-5">
 
-            <div className={`${output||loading && "md:grid grid-cols-2"}  md:w-[60%] w-[85%] gap-5`}>
-                <textarea placeholder="好きな言語で入力..." className="bg-white w-full h-72 rounded-xl p-2 outline-none" onChange={(e) => setInputText(e.target.value)} value={inputText} />
+            <div className={`${(output||loading) && "md:grid grid-cols-2"}  md:w-[60%] w-[85%] gap-5`}>
+                <textarea placeholder="好きな言語で入力..." className="bg-white w-full md:h-72 rounded-xl p-2 outline-none mb-5" onChange={(e) => setInputText(e.target.value)} value={inputText} />
 
 
                 {loading ?
@@ -72,22 +52,22 @@ const TextInputField = () => {
                         <span className="dot">.</span></p>   
                         </div>
                     : output ?
-                        <div className=" flex items-start justify-start flex-col gap-5">
-                            <div className="text-left bg-red-300 p-2 rounded-lg text-white font-semibold">
+                        <div className=" flex items-start justify-start flex-col gap-5 ">
+                            <div className="text-left bg-red-300 p-2 rounded-lg text-white font-semibold w-full">
                                 <p>Translation</p>
                                 <p>{translation}</p>
                             </div>
 
-                            <div className="text-left bg-red-300 p-2 rounded-lg text-white font-semibold">
+                            <div className="text-left bg-red-300 p-2 rounded-lg text-white font-semibold w-full">
                                 <p>Meaning</p>
                                 <p>{meaning}</p>
                             </div>
 
-                            <div className="text-left bg-red-300 p-2 rounded-lg text-white font-semibold">
+                            <div className="text-left bg-red-300 p-2 rounded-lg text-white font-semibold w-full">
                                 <p>Explanation</p>
                                 <ul>
                                     {explanations.map((exp: string[], index: number) => (
-                                        <li className="mb-2" key={index}>{exp}</li>
+                                        <li className="mb-2 " key={index}>・{exp}</li>
                                     ))}
                                 </ul>
 
@@ -99,18 +79,10 @@ const TextInputField = () => {
 
             <div className="flex flex-row">
                 <RecordButton />
-                <div>
-                    <p>入力言語を選択</p>
-                    <select name="" id="" onChange={(e) => handleSelection(e)} className="rounded-xl py-1 ">
-                        <option value="English">English</option>
-                        <option value="Chinese" >Chinese</option>
-                        <option value="Korean" >Korean</option>
-                        <option value="Japanese" >Japanese</option>
-                    </select>
-                </div>
+               
             </div>
 
-            <button onClick={handleDatafetch} className={`${inputText ? "bg-red-300 cursor-pointer " : "bg-slate-300 cursor-default "} md:w-[60%] w-[85%] py-3 rounded-2xl font-semibold text-white  outline-none`}>翻訳する ↑</button>
+            <button onClick={getTranslationData} className={`${inputText ? "bg-red-300 cursor-pointer " : "bg-slate-300 cursor-default "} md:w-[60%] w-[85%] py-3 rounded-2xl font-semibold text-white  outline-none`}>翻訳する ↑</button>
 
         </div>
 

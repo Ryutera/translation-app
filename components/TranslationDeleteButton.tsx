a46735f23c65f17a2bca55cec0f8d53157a94b0f
Ryutera <kuya9823@gmail.com>
2025-12-11 12:18:15 +0000

    window confirm replaced by alert dialog

--- components/TranslationDeleteButton.tsx
+++ components/TranslationDeleteButton.tsx
@@ -1,22 +1,53 @@
 "use client"
 
+
 import { deleteTranslationHistory } from "@/app/action"
 import { useRouter } from "next/navigation"
+import { Button } from "./ui/button"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog"
 
 
- const TranslationDeleteButton  = ({id}:{id:string})=>{
-    const router = useRouter()
-    const handleDelete = async()=>{
-        const confirm = window.confirm("Are you to delete it?")
+const TranslationDeleteButton = ({ id }: { id: string }) => {
+  const router = useRouter()
+
+  const handleDelete = async () => {
+
+      await deleteTranslationHistory(id)
+      router.refresh()
     
+  }
 
-        if (confirm) {
-            await deleteTranslationHistory(id)
-            router.refresh()
-        }
-    }
-  
-  return (<button className="z-10 h-full  px-3" onClick={handleDelete}>...</button>)
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+       
+          <button className="z-10 h-full  px-3" >...</button>
+        
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure to delete it?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={handleDelete}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
 }
 
 export default TranslationDeleteButton 
